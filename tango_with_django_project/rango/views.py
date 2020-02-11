0d5e422b15185457aab705c07b2c446ca6d98952
Peter Dodd <2308057d@student.gla.ac.uk>
2020-02-11 18:42:20 +0000

    chapter 4

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -2,8 +2,16 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Rango says hey there partner! <a href='/rango/about/'>About</a>)")
+    # Dictionary to pass to the template engine as its context.
+    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+
+    # Returns rendered response to send to client
+    return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
-    return HttpResponse("Rango says here is the about page! <a href='/rango/'>Index</a>")
+    # Dictionary to pass to the template engine as its context.
+    context_dict = {'boldmessage': 'This tutorial has been put together by Peter Dodd'}
+
+    # Returns rendered response to send to client
+    return render(request, 'rango/about.html', context=context_dict)
 
