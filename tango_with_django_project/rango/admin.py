29f2b67cf4aed64a8170160f41518c5c085f0317
Peter Dodd <2308057d@student.gla.ac.uk>
2020-02-14 16:45:18 +0000

    chapter 5

--- tango_with_django_project/rango/admin.py
+++ tango_with_django_project/rango/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from rango.models import Category, Page, Question, Choice
 
-# Register your models here.
+
+class ChoiceInLine(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [(None, {'fields': ['question_text']}),
+                 ('Date information', {'fields': ['pub_date']})]
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
+
+
+class PageAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'url')
+
+
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Category)
+admin.site.register(Page, PageAdmin)
